a3d82d3af8ce18186d223f0acaf7db6b88b9974c
ArtCorner <info@artcorner.hu>
2022-11-02 19:42:26 +0100

    add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { driverRouter } from "./controllers/driver.router";
 
 dotenv.config();
 const app = express();
 app.use(express.static("public"));
+app.use(cors());
 
 app.use(express.json({ limit: "1mb" }));
 app.use("/api/drivers", driverRouter);
