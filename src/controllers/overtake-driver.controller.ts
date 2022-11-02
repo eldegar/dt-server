f37266baf32b67cc2e45f1253473931cc1832f3a
ArtCorner <info@artcorner.hu>
2022-11-02 15:04:05 +0100

    refactor controller

--- src/controllers/overtake-driver.controller.ts
+++ src/controllers/overtake-driver.controller.ts
@@ -1,27 +1,16 @@
 import { Request, Response } from "express";
-import { driversData } from "../database/db";
+import { findDriverIndexById, swapDriver } from "./driver.dao";
 
 export function overTakeDriverController(req: Request, res: Response) {
   if (typeof req.params.driverId === "undefined") {
     res.send(404).end();
   }
 
-  const driverIndex = driversData.findIndex(
-    (driver) => driver.id === parseInt(req.params.driverId)
-  );
+  const driverIndex = findDriverIndexById(parseInt(req.params.driverId));
 
   if (driverIndex === 0) {
     res.sendStatus(403).end();
   } else {
-    swapDriver(driverIndex);
-    res.send(driversData);
+    res.send(swapDriver(driverIndex));
   }
-}
-
-function swapDriver(driverIndex: number) {
-  driversData[driverIndex].place--;
-  driversData[driverIndex - 1].place++;
-
-  const driverStartOvertake = driversData.splice(driverIndex, 1);
-  driversData.splice(driverIndex - 1, 0, driverStartOvertake[0]);
 }
