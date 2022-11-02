f0d6e91a91c93c80cd32815aef7a43edce4ff350
ArtCorner <info@artcorner.hu>
2022-11-02 15:15:13 +0100

    fix response status if parameter is invalid

--- src/controllers/overtake-driver.controller.ts
+++ src/controllers/overtake-driver.controller.ts
@@ -3,13 +3,13 @@ import { findDriverIndexById, swapDriver } from "./driver.dao";
 
 export function overTakeDriverController(req: Request, res: Response) {
   if (typeof req.params.driverId === "undefined") {
-    res.send(404).end();
+    res.sendStatus(404).end();
   }
 
   const driverIndex = findDriverIndexById(parseInt(req.params.driverId));
 
-  if (driverIndex === 0) {
-    res.sendStatus(403).end();
+  if (driverIndex <= 0) {
+    res.sendStatus(406).end();
   } else {
     res.send(swapDriver(driverIndex));
   }
