ed25bd65a4029d225effe20261aebd25388d39fc
ArtCorner <info@artcorner.hu>
2022-11-02 21:22:37 +0100

    change drivers data source

--- src/controllers/driver.dao.ts
+++ src/controllers/driver.dao.ts
@@ -1,18 +1,18 @@
-import { driversData } from "../database/db";
+import { jsonDb } from "../database/jsondb";
 
 export function findDriverIndexById(driverId: number) {
-  return driversData.findIndex((driver) => driver.id === driverId);
+  return jsonDb.drivers.findIndex((driver) => driver.id === driverId);
 }
 
 export function swapDriver(driverIndex: number) {
-  driversData[driverIndex].place--;
-  driversData[driverIndex - 1].place++;
+  jsonDb.drivers[driverIndex].place--;
+  jsonDb.drivers[driverIndex - 1].place++;
 
-  const driverStartOvertake = driversData.splice(driverIndex, 1);
-  driversData.splice(driverIndex - 1, 0, driverStartOvertake[0]);
-  return driversData;
+  const driverStartOvertake = jsonDb.drivers.splice(driverIndex, 1);
+  jsonDb.drivers.splice(driverIndex - 1, 0, driverStartOvertake[0]);
+  return jsonDb.drivers;
 }
 
 export function getDrivers() {
-  return driversData;
+  return jsonDb.drivers;
 }
