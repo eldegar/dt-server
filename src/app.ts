19d2ba514a31fc7e8a38da1e591be666f3ee870b
ArtCorner <info@artcorner.hu>
2022-11-02 23:48:48 +0100

    add logger and log requests to console

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { driverRouter } from "./controllers/driver.router";
+import { logRequest } from "./middleware/log-request";
 
 dotenv.config();
 const app = express();
 app.use(express.static("public"));
 app.use(cors());
+app.use(logRequest);
 
 app.use(express.json({ limit: "1mb" }));
 app.use("/api/drivers", driverRouter);
